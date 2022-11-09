c8b3a49c7ecddd30debdbcfbb3e40fec6148f577
makoysof <makoytan11@gmail.com>
2022-11-09 16:34:13 +0800

    Added Search function. Made adjustments on scale of images shown.

--- app/src/main/java/com/example/esciencenewsapp/MainActivity.java
+++ app/src/main/java/com/example/esciencenewsapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.esciencenewsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -9,6 +10,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.esciencenewsapp.Models.ApiResponse;
 import com.example.esciencenewsapp.Models.Headlines;
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity implements SelectedListener,
     RecyclerView recyclerView;
     NewsAdapter newsAdapter;
     Button general, business, health, technology, sports, science, entertainment;
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,17 +40,37 @@ public class MainActivity extends AppCompatActivity implements SelectedListener,
         sports = findViewById(R.id.sports);
         science = findViewById(R.id.science);
         entertainment = findViewById(R.id.entertainment);
+        searchView = findViewById(R.id.searchView);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                RequestManager requestManager = new RequestManager(MainActivity.this);
+                requestManager.getNews(listener, "general", query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
     }
 
     private final OnFetchDataListener<ApiResponse> listener = new OnFetchDataListener<ApiResponse>() {
         @Override
         public void onFetchData(List<Headlines> list, String message) {
-            showNews(list);
+            if(list.isEmpty()){
+                Toast.makeText(MainActivity.this, "No article found", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                showNews(list);
+            }
         }
 
         @Override
         public void onErrorListener(String message) {
-
+            Toast.makeText(MainActivity.this, "Error Occurred!", Toast.LENGTH_SHORT).show();
         }
     };
 
